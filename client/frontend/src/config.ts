db68a12a9d2c22d22550cf128cd3834ff5d8bac8
morax <ld.seraph@163.com>
2022-10-14 19:06:24 +0800

    new file:   client/.gitignore 	new file:   client/Makefile 	new file:   client/app.go 	new file:   client/build/README.md 	new file:   client/build/appicon.png 	new file:   client/build/darwin/Info.plist 	new file:   client/build/windows/icon.ico 	new file:   client/build/windows/info.json 	new file:   client/build/windows/installer/project.nsi 	new file:   client/build/windows/installer/wails_tools.nsh 	new file:   client/build/windows/wails.exe.manifest 	new file:   client/frontend/.editorconfig 	new file:   client/frontend/.eslintignore 	new file:   client/frontend/.eslintrc.js 	new file:   client/frontend/.gitignore 	new file:   client/frontend/.npmrc 	new file:   client/frontend/.prettierrc 	new file:   client/frontend/.vscode/extensions.json 	new file:   client/frontend/.vscode/settings.json 	new file:   client/frontend/README.md 	new file:   client/frontend/index.html 	new file:   client/frontend/package.json 	new file:   client/frontend/package.json.md5 	new file:   client/frontend/postcss.config.js 	new file:   client/frontend/public/favicon.ico 	new file:   client/frontend/public/icons/favicon-128x128.png 	new file:   client/frontend/public/icons/favicon-16x16.png 	new file:   client/frontend/public/icons/favicon-32x32.png 	new file:   client/frontend/public/icons/favicon-96x96.png 	new file:   client/frontend/quasar.config.js 	new file:   client/frontend/src/App.vue 	new file:   client/frontend/src/api/wailsjs/go/main/App.d.ts 	new file:   client/frontend/src/api/wailsjs/go/main/App.js 	new file:   client/frontend/src/api/wailsjs/runtime/package.json 	new file:   client/frontend/src/api/wailsjs/runtime/runtime.d.ts 	new file:   client/frontend/src/api/wailsjs/runtime/runtime.js 	new file:   client/frontend/src/assets/quasar-logo-vertical.svg 	new file:   client/frontend/src/boot/.gitkeep 	new file:   client/frontend/src/boot/i18n.ts 	new file:   client/frontend/src/components/EssentialLink.vue 	new file:   client/frontend/src/components/ExampleComponent.vue 	new file:   client/frontend/src/components/models.ts 	new file:   client/frontend/src/config.ts 	new file:   client/frontend/src/css/app.scss 	new file:   client/frontend/src/css/quasar.variables.scss 	new file:   client/frontend/src/env.d.ts 	new file:   client/frontend/src/i18n/en-US/index.ts 	new file:   client/frontend/src/i18n/index.ts 	new file:   client/frontend/src/layouts/MainLayout.vue 	new file:   client/frontend/src/pages/ErrorNotFound.vue 	new file:   client/frontend/src/pages/IndexPage.vue 	new file:   client/frontend/src/quasar.d.ts 	new file:   client/frontend/src/router/index.ts 	new file:   client/frontend/src/router/routes.ts 	new file:   client/frontend/src/shims-vue.d.ts 	new file:   client/frontend/src/stores/example-store.ts 	new file:   client/frontend/src/stores/index.ts 	new file:   client/frontend/src/stores/store-flag.d.ts 	new file:   client/frontend/tsconfig.json 	new file:   client/frontend/yarn.lock 	new file:   client/go.mod 	new file:   client/go.sum 	new file:   client/main.go 	new file:   client/wails.json

---
+++ client/frontend/src/config.ts
@@ -0,0 +1,5 @@
+export default {
+  app: {
+    name: '设备发现工具',
+  }
+}
---
+++ client/frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-bar style="--wails-draggable:drag">
+        <q-icon name="laptop_chromebook" />
+        <div>{{ config.app.name }}</div>
+
+        <q-space />
+
+        <q-btn dense flat icon="minimize" @click="WindowMinimise" />
+        <q-btn dense flat icon="crop_square"
+          @click="WindowToggleMaximise" />
+        <q-btn dense flat icon="close" @click="Quit" />
+      </q-bar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import config from '@/config'
+import { WindowMinimise, WindowToggleMaximise, Quit } from '@/api/wailsjs/runtime'
+</script>
